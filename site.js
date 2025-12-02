3e9711bf3dc6c89830c46f41df77e309cbfb28b7
alexagold1 <526agold@frhsd.com>
2025-12-02 10:08:16 -0500

    added step three additional variable and did the ownership code in JS

--- site.js
+++ site.js
@@ -20,26 +20,34 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
-
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
-      },
-      data() {
+  // This automatically imports your movies.json file and puts it into
+  //   the variable: movies
+  created() {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((json) => {
+        this.movies = json;
+      });
+  },
+  data() {
+    return {
         return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
-      }
-    },
-      methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
-})
+            title: "Alexa's Top 8 Movies",
+            owner: "Alexa",
+            github: "https://github.com/alexagold1",
+            movies: []
+        };
+      },
+      created() {
+        fetch('movies.json')
+        .then(response => response.json())
+        .then(json => { this.movies = json; });
+      },
+  },
+  methods: {
+    /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+  },
+});
 
-vue_app.mount("#vue_app")
+vue_app.mount("#vue_app");
